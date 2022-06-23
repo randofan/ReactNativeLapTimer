6d29a8a30b2ab43daae04dc6639bb69796e83a0f
randofan <davidsong.98075@gmail.com>
2022-06-23 14:14:47 -0700

    android release works

--- android/app/src/main/java/com/myapp/MainActivity.java
+++ android/app/src/main/java/com/myapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.myapp;
+package com.davsong.laptimer;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- android/app/src/main/java/com/myapp/MainApplication.java
+++ android/app/src/main/java/com/myapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.myapp;
+package com.davsong.laptimer;
 
 import android.app.Application;
 import android.content.Context;
@@ -10,7 +10,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
-import com.myapp.newarchitecture.MainApplicationReactNativeHost;
+import com.davsong.laptimer.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.reactnativecommunity.picker.RNCPickerPackage;
@@ -75,7 +75,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.myapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.davsong.laptimer.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
